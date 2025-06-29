b1053b2f6fbb8482d71b082695bd073996a91e45
Saroj Paudel <sarozpaudel6@gmail.com>
2025-06-29 23:15:20 +0545

    Day 4: Completed !

---
+++ day_4/readingFiles.py
@@ -0,0 +1,16 @@
+# f = open("file.txt","r")
+# f.close()
+
+
+# with open('file.txt','r') as f:
+#     content = f.read()
+#     content = f.readline()
+#     print(content)
+
+
+# with open('file1.txt','w') as f:
+#     f.write("Test")
+    
+with open('file1.txt','a') as f:
+    f.write("123")
+    
